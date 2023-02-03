7d5f21e4dabc8eb5aab2c409836bca5ddc151be5
KyeongRyeol <rlarudfuf802@naver.com>
2023-02-04 02:57:45 +0900

    Apply .gitignore

--- src/com/newlecture/web/info/PrivateInfo.java
+++
@@ -1,9 +0,0 @@
-package com.newlecture.web.info;
-
-public class PrivateInfo {
-    public static final String url = "jdbc:oracle:thin:@localhost:1521/xepdb1";
-    public static final String uid = "sys as sysdba";
-    public static final String pwd = "kkr983224871!";
-    public static final String driver = "oracle.jdbc.driver.OracleDriver";
-
-}
