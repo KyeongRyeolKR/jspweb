d14b1b5b479a4c79fdf9b6d2aaa31fcdf10fbd1e
KyeongRyeol <rlarudfuf802@naver.com>
2023-02-06 00:45:40 +0900

    23-02-05 / 04:02 JSP 웹 게시판 만들기  - BACK : 검색 기능 추가, 페이징 기능  - NOW : 게시글 상세보기 수정  - NEXT :

--- src/com/newlecture/web/controller/NoticeListController.java
+++ src/com/newlecture/web/controller/NoticeListController.java
@@ -21,10 +21,33 @@ import java.util.List;
 public class NoticeListController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        // list?f=title&q=a
+        String field_ = request.getParameter("f");
+        String query_ = request.getParameter("q");
+        String page_ = request.getParameter("p");
+
+        String field = "title";     // 기본값
+        if(field_ != null && !field_.equals("")){
+            field = field_;
+        }
+
+        String query = "";          // 기본값
+        if(query_ != null && !query_.equals("")){
+            query = query_;
+        }
+
+        int page = 1;          // 기본값
+        if(page_ != null && !page_.equals("")){
+            page = Integer.parseInt(page_);
+        }
+
         NoticeService service = new NoticeService();
-        List<Notice> list = service.getNoticeList();
+        List<Notice> list = service.getNoticeList(field, query, page);
+        int count = service.getNoticeCount(field, query);
 
         request.setAttribute("list", list);
+        request.setAttribute("count", count);
 
         request.getRequestDispatcher("/WEB-INF/view/notice/list.jsp").forward(request, response);
 
